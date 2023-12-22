b755bac2362d2d84f9eb7482928a265d9934c955
110360136 <t110360136@ntut.org.tw>
2023-12-22 13:41:57 +0800

    20231222

--- app/src/main/java/com/example/lab11/MainActivity.java
+++ app/src/main/java/com/example/lab11/MainActivity.java
@@ -43,79 +43,86 @@ public class MainActivity extends AppCompatActivity {
         dbrw = new MyDBHelper(this).getWritableDatabase();
 
         btn_insert.setOnClickListener(view -> {
-            if (ed_book.length() <1 || ed_price.length() <1)
-                Toast.makeText( MainActivity.this, "欄位請物留空", Toast.LENGTH_SHORT).show();
+            if (ed_book.length() < 1 || ed_price.length() < 1)
+                Toast.makeText(MainActivity.this, "欄位請物留空", Toast.LENGTH_SHORT).show();
             else {
                 try {
-                    dbrw.execSQL(  "INSERT INTO myTable(book,price) values(?.?)",
+                    dbrw.execSQL("INSERT INTO myTable(book,price) values(?,?)",
                             new Object[]{ed_book.getText().toString(), ed_price.getText().toString()});
-                    Toast.makeText( MainActivity.this, "新增書名" + ed_book.getText().toString() +
-                            "價格"+ ed_price.getText().toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "新增書名" + ed_book.getText().toString() +
+                            "價格" + ed_price.getText().toString(), Toast.LENGTH_SHORT).show();
                     ed_book.setText("");
                     ed_price.setText("");
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText( MainActivity.this, "新增失敗" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "新增失敗" + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
         btn_update.setOnClickListener(view -> {
-            if (ed_book.length() <1 || ed_price.length() <1)
-                Toast.makeText( MainActivity.this, "欄位請物留空", Toast.LENGTH_SHORT).show();
+            if (ed_book.length() < 1 || ed_price.length() < 1)
+                Toast.makeText(MainActivity.this, "欄位請物留空", Toast.LENGTH_SHORT).show();
             else {
                 try {
-                    dbrw.execSQL(  "UPDATE myTable SET price= "+ed_price.getText().toString()+ "WHERE book LIKE'" + ed_book.getText().toString()+ "'");
-                    Toast.makeText( MainActivity.this, "更新書名" + ed_book.getText().toString() +
-                            "價格"+ ed_price.getText().toString(), Toast.LENGTH_SHORT).show();
+                    dbrw.execSQL("UPDATE myTable SET price = " + ed_price.getText().toString() + " WHERE book LIKE '" + ed_book.getText().toString() + "'");
+                    Toast.makeText(MainActivity.this, "更新書名" + ed_book.getText().toString() +
+                            "價格" + ed_price.getText().toString(), Toast.LENGTH_SHORT).show();
                     ed_book.setText("");
                     ed_price.setText("");
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText( MainActivity.this, "更新失敗" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "更新失敗" + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
         btn_delete.setOnClickListener(view -> {
-            if (ed_book.length() <1 )
-                Toast.makeText( MainActivity.this, "書名請物留空", Toast.LENGTH_SHORT).show();
+            if (ed_book.length() < 1)
+                Toast.makeText(MainActivity.this, "書名請物留空", Toast.LENGTH_SHORT).show();
             else {
                 try {
-                    dbrw.execSQL(  "DELETE FROM myTable WHERE book LIKE= "+ed_book.getText().toString()+ "'");
-                    Toast.makeText( MainActivity.this, "刪除書名" + ed_book.getText().toString(),
-                           Toast.LENGTH_SHORT).show();
+                    dbrw.execSQL("DELETE FROM myTable WHERE book LIKE '" + ed_book.getText().toString() + "'");
+                    Toast.makeText(MainActivity.this, "刪除書名" + ed_book.getText().toString(),
+                            Toast.LENGTH_SHORT).show();
                     ed_book.setText("");
                     ed_price.setText("");
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText( MainActivity.this, "刪除失敗" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "刪除失敗" + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
         btn_query.setOnClickListener(view -> {
             Cursor c;
-            if (ed_book.length() <1 )
-                c = dbrw.rawQuery( "SELECT * FROM myTable",  null);
+            if (ed_book.length() < 1)
+                c = dbrw.rawQuery("SELECT * FROM myTable", null);
             else
-                c = dbrw.rawQuery( "SELECT * FROM myTable WHERE bool LIKE '"+ed_book.getText().toString()+ "'",  null);
+                c = dbrw.rawQuery("SELECT * FROM myTable WHERE book LIKE '" + ed_book.getText().toString() + "'", null);
 
             c.moveToFirst();
             items.clear();
-            Toast.makeText( MainActivity.this, "共有" + c.getCount() + "筆",
-                            Toast.LENGTH_SHORT).show();
-             for (int i = 0; i < c.getCount(); i++){
-                 items.add("書籍" + c.getCount(0))
-             }
-
+            Toast.makeText(MainActivity.this, "共有" + c.getCount() + "筆",
+                    Toast.LENGTH_SHORT).show();
+            for (int i = 0; i < c.getCount(); i++) {
+                items.add("書籍" + c.getString(0) + "\t\t\t\t價格:" + c.getString(1));
+                c.moveToNext();
+            }
+            adapter.notifyDataSetChanged();
+            c.close();
 
         });
 
     }
+
+
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
 
         dbrw.close();
     }
+
+}
+
